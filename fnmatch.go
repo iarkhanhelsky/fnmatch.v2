626d489b67a61a47c4e7d29b552b915c2755687c
Ilya Arkhanhelsky <ilya.arkhanhelsky@gmail.com>
2022-02-02 20:20:35 +0200

    Started implementing fnmatchHelper

--- fnmatch.go
+++ fnmatch.go
@@ -1,5 +1,10 @@
 package fnmatch
 
+import (
+	"unicode"
+	"unicode/utf8"
+)
+
 const (
 	// FNM_NOESCAPE If this flag is set, treat backslash as an ordinary
 	// character, instead of an escape character.
@@ -50,12 +55,119 @@ const (
 	//		with any of the patterns in the pattern-list.
 	FNM_EXTMATCH
 
+	FNM_DOTMATCH   = FNM_PERIOD
 	FNM_IGNORECASE = FNM_CASEFOLD
 	FNM_FILE_NAME  = FNM_PATHNAME
 )
 
 // Matches the pattern against the string, with the given flags, and returns true if the match is
 // successful.
-func Match(pattern, s string, flags int) bool {
-	return false
+func Match(pattern string, str string, flags int) bool {
+	pathname := hasFlag(flags, FNM_PATHNAME)
+
+	if pathname {
+		// TODO
+		return false
+	}
+
+	return fnmatchHelper(pattern, str, flags)
+}
+
+func fnmatchHelper(pattern string, str string, flags int) bool {
+	period := !hasFlag(flags, FNM_DOTMATCH)
+
+	px := 0
+	sx := 0
+
+	ptmp := -1
+	stmp := -1
+
+	var prn, srn rune
+	var psz, ssz int
+
+	if period && str[sx] == '.' && pattern[unescape(px, pattern, flags)] != '.' {
+		return false
+	}
+
+	for {
+		switch pattern[px] {
+		case '*':
+			for pattern[px] == '*' {
+				px++
+			}
+
+			if isEnd(unescape(px, pattern, flags), pattern, flags) {
+				px = unescape(px, pattern, flags)
+				return false
+			}
+
+			if isEnd(sx, str, flags) {
+				return false
+			}
+
+			ptmp = px
+			stmp = sx
+		case '?':
+			if isEnd(sx, str, flags) {
+				return false
+			}
+
+			px++
+			_, ssz = utf8.DecodeRuneInString(str[stmp:])
+			sx += ssz
+			continue
+		}
+
+		px = unescape(px, pattern, flags)
+		if isEnd(sx, str, flags) {
+			return isEnd(px, pattern, flags)
+		}
+		if isEnd(px, pattern, flags) {
+			goto failed
+		}
+
+		prn, psz = utf8.DecodeRuneInString(pattern[px:])
+		srn, ssz = utf8.DecodeRuneInString(str[sx:])
+		if psz == ssz && prn == srn {
+			px += psz
+			sx += ssz
+		}
+
+		if !hasFlag(flags, FNM_CASEFOLD) {
+			goto failed
+		}
+
+		if unicode.ToUpper(prn) != unicode.ToUpper(srn) {
+			goto failed
+		}
+		px += psz
+		sx += ssz
+		continue
+
+	failed:
+		// try next '*' position
+		if ptmp >= 0 && stmp >= 0 {
+			px = ptmp
+			_, ssz = utf8.DecodeRuneInString(str[stmp:])
+			sx += ssz
+		}
+
+		return false
+	}
+}
+
+func hasFlag(mask int, flag int) bool {
+	return mask&flag != 0
+}
+
+func unescape(p int, pattern string, flags int) int {
+	if !hasFlag(flags, FNM_NOESCAPE) && pattern[p] == '\\' {
+		return p + 1
+	}
+
+	return p
+}
+
+func isEnd(p int, pattern string, flags int) bool {
+	return p >= len(pattern) || (hasFlag(flags, FNM_PATHNAME) && pattern[p] == '/')
 }
