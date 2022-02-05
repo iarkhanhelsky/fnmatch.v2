5ac09b23cac3153d2fe0ae161e84f30c29684398
Ilya Arkhanhelsky <ilya.arkhanhelsky@gmail.com>
2022-02-05 11:03:01 +0200

    Updated report.sh

--- report.sh
+++ report.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo ":: GO"
-go test ./... | grep FAIL: | sort
+GO_REPORT=$(go test ./... | grep FAIL: | sort | grep -o -e 'test_[^ ]*')
+RB_REPORT=$(ruby fnmatch_test.rb | grep Failure | sort | grep -o -e 'test_[^(]*')
 
-echo ":: Ruby"
-ruby fnmatch_test.rb | grep Failure | sort
+echo "diff RB GO"
+diff -U 8 <(echo "$RB_REPORT") <(echo "$GO_REPORT") | sed 's|--- /dev/[^ ]*|--- ruby  |' | sed 's|+++ /dev/[^ ]*|+++ golang|'
