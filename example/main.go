3ee7408d11aaea8beb31fcb05d75091d4c65f5ee
Ilya Arkhanhelsky <ilya.arkhanhelsky@gmail.com>
2022-02-05 19:18:08 +0200

    Add go actions

--- example/main.go
+++ example/main.go
@@ -1,9 +1,12 @@
-package example
+package main
 
 import "github.com/iarkhanhelsky/fnmatch.v2"
 
 func main() {
 	if fnmatch.Match("foo/*", "foo/bar") {
 		println("Matched!")
+		return
 	}
+
+	panic("Fail, not matched")
 }
