2403cde9629a0e3c7369f28a68f349522a8ac03b
Ilya Arkhanhelsky <ilya.arkhanhelsky@gmail.com>
2022-02-05 11:15:45 +0200

    Update report.sh to support colordiff

--- report.sh
+++ report.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+cdiff='diff'
+if [[ ! -z "$(command -v colordiff)" ]]; then
+  cdiff='colordiff'
+fi
+
 GO_REPORT=$(go test ./... | grep FAIL: | sort | grep -o -e 'test_[^ ]*')
 RB_REPORT=$(ruby fnmatch_test.rb | grep Failure | sort | grep -o -e 'test_[^(]*')
 
-echo "diff RB GO"
-diff -U 8 <(echo "$RB_REPORT") <(echo "$GO_REPORT") | sed 's|--- /dev/[^ ]*|--- ruby  |' | sed 's|+++ /dev/[^ ]*|+++ golang|'
+$cdiff -U 8 <(echo "$RB_REPORT") <(echo "$GO_REPORT") | sed 's|--- /dev/[^ ]*|--- ruby  |' | sed 's|+++ /dev/[^ ]*|+++ golang|'
